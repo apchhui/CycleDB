d1a4a87321996fc2ad05dd69473598baa0495817
apchhui <apchihbus23@gmail.com>
2025-01-15 21:15:19 +0300

    Full Project

---
+++ build/CMakeFiles/Database.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Database.dir/src/Database.cpp.obj"
+  "CMakeFiles/Database.dir/src/Database.cpp.obj.d"
+  "CMakeFiles/Database.dir/src/helpers.cpp.obj"
+  "CMakeFiles/Database.dir/src/helpers.cpp.obj.d"
+  "CMakeFiles/Database.dir/src/main.cpp.obj"
+  "CMakeFiles/Database.dir/src/main.cpp.obj.d"
+  "Database.exe"
+  "Database.exe.manifest"
+  "Database.pdb"
+  "libDatabase.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Database.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
